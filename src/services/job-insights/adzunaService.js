8d73588de078179c12211d7bc70e8852a8812d13
techprevail-hub <tech@withprevail.com>
2026-06-18 18:44:25 +0530

    refactor job insights service: simplify job fetching and remove unused logic

--- src/services/job-insights/adzunaService.js
+++ src/services/job-insights/adzunaService.js
@@ -2,23 +2,30 @@ import axios from "axios";
 
 export const fetchJobsFromAdzuna = async () => {
   try {
+
+    console.log("APP ID:", process.env.ADZUNA_APP_ID);
+    console.log("APP KEY:", process.env.ADZUNA_APP_KEY);
+
     const response = await axios.get(
-      "https://api.adzuna.com/v1/api/jobs/in/search/1",
+      "https://api.adzuna.com/v1/api/jobs/in/in/search/1",
       {
         params: {
           app_id: process.env.ADZUNA_APP_ID,
           app_key: process.env.ADZUNA_APP_KEY,
           results_per_page: 20,
+          what: "software developer",
         },
       }
     );
 
-    console.log("Adzuna Response:", response.data);
+    console.log("TOTAL JOBS:", response.data.results?.length);
 
     return response.data.results || [];
+
   } catch (error) {
-    console.error("Adzuna Error:");
-    console.error(error.response?.data || error.message);
+
+    console.log("===== ADZUNA ERROR =====");
+    console.log(error.response?.data || error.message);
 
     return [];
   }
